251be4f7ce0a852ba0b86d7a68c97d127f070f75
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-17 20:54:17 +0200

    ts component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   welcome = "Olá, seja muito bem-vindo(a)!";
+  sites = ["https://app.perfectlyspoken.com/auth/login","https://docs.google.com/forms/d/e/1FAIpQLSfm06F8tCScvMNVBg8H9OJumpAgOU6GcLMGoLN_ehPErzY3yA/viewform","https://www.ru.nl/en/education/masters/cognitive-neuroscience-research","https://www.ucl.ac.uk"];
+  item = this.sites[0];
+  items = document.getElementById('link');
+
+  public i: number;
+  public nums: Array<number>;
+  public strLink!: string;
+
+  constructor() {
+    this.i = -1;
+    this.nums = [0,1,2,3];
+  }
+  
+  myFunc(){
+    this.i++
+    if(this.i === 4){
+      this.i = 0
+    }
+    console.log(this.sites[this.nums[this.i]])
+    this.strLink = this.sites[this.nums[this.i]];
+    
+    document.getElementById('link')?.setAttribute('href', this.strLink)
+    
+  }  
 }
+
+
+
