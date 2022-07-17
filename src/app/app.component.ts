14c28024e323fb00810f8d22dabd51b2fdb97379
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-17 13:48:31 +0200

    update ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'estudo-lingui';
+  welcome = "Olá, seja muito bem-vindo(a)!";
 }
