5dfa4d0d00dbb0fc515d13949d0d46677783f49e
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-25 16:40:59 +0200

    update list array by removing 1 temporaly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   welcome = "Olá, seja muito bem-vindo(a)!";
-  sites = ["https://docs.google.com/forms/d/e/1FAIpQLSdaDNk7Z9R1-yCwB-PlT1KrV1P6sS3wkZqjCGQTiLQIdR_OuQ/viewform?usp=sf_link","https://docs.google.com/forms/d/e/1FAIpQLSd7ZhU5z7dpKjX1R1u8LK9z78oW6i-JLCOCsCmbYT6PIo3dIg/viewform?usp=sf_link"];
+  sites = ["https://docs.google.com/forms/d/e/1FAIpQLSdaDNk7Z9R1-yCwB-PlT1KrV1P6sS3wkZqjCGQTiLQIdR_OuQ/viewform?usp=sf_link","https://docs.google.com/forms/d/e/1FAIpQLSewfmrpTfdi4b4Uekee5o4_k9ZCprVxWAVVoI9F5BlqELTR3Q/viewform?usp=sf_link"];
   strLink!: string;
   
   myFunc(){
