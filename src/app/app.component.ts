69216e1e22a49949cf7023986d33049e5959c287
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-25 16:45:58 +0200

    update list array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   welcome = "Olá, seja muito bem-vindo(a)!";
-  sites = ["https://docs.google.com/forms/d/e/1FAIpQLSdaDNk7Z9R1-yCwB-PlT1KrV1P6sS3wkZqjCGQTiLQIdR_OuQ/viewform?usp=sf_link","https://docs.google.com/forms/d/e/1FAIpQLSewfmrpTfdi4b4Uekee5o4_k9ZCprVxWAVVoI9F5BlqELTR3Q/viewform?usp=sf_link"];
+  sites = ["https://docs.google.com/forms/d/e/1FAIpQLSdaDNk7Z9R1-yCwB-PlT1KrV1P6sS3wkZqjCGQTiLQIdR_OuQ/viewform?usp=sf_link","https://docs.google.com/forms/d/e/1FAIpQLSfm06F8tCScvMNVBg8H9OJumpAgOU6GcLMGoLN_ehPErzY3yA/viewform?usp=sf_link"];
   strLink!: string;
   
   myFunc(){
