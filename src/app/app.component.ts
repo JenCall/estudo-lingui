441cbed8ab01c622d93f7f1d636c3d2f688051e9
Jeniffer Callou <jenifferprc@gmail.com>
2022-07-25 13:16:09 +0200

    update list array by removing 1 temporaly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   welcome = "Olá, seja muito bem-vindo(a)!";
-  sites = ["https://docs.google.com/forms/d/e/1FAIpQLSfm06F8tCScvMNVBg8H9OJumpAgOU6GcLMGoLN_ehPErzY3yA/viewform?usp=sf_link","https://docs.google.com/forms/d/e/1FAIpQLSdaDNk7Z9R1-yCwB-PlT1KrV1P6sS3wkZqjCGQTiLQIdR_OuQ/viewform?usp=sf_link","https://docs.google.com/forms/d/e/1FAIpQLSd7ZhU5z7dpKjX1R1u8LK9z78oW6i-JLCOCsCmbYT6PIo3dIg/viewform?usp=sf_link"];
+  sites = ["https://docs.google.com/forms/d/e/1FAIpQLSdaDNk7Z9R1-yCwB-PlT1KrV1P6sS3wkZqjCGQTiLQIdR_OuQ/viewform?usp=sf_link","https://docs.google.com/forms/d/e/1FAIpQLSd7ZhU5z7dpKjX1R1u8LK9z78oW6i-JLCOCsCmbYT6PIo3dIg/viewform?usp=sf_link"];
   strLink!: string;
   
   myFunc(){
   // Random approach 
-    this.strLink = this.sites[Math.floor(Math.random() * (2 + 1))];
+    this.strLink = this.sites[Math.floor(Math.random() * (1 + 1))];
   // sending to html
     document.getElementById('link')?.setAttribute('href', this.strLink)
   } 
